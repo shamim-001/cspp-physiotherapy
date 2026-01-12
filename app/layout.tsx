3d7acf18c72e3ff7b4fb718fcbdbd32668726374
shamim-001 <s.ahsan.ds96@gmail.com>
2026-01-12 07:48:52 +0600

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,8 +32,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", sizes: "48x48", type: "image/x-icon" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
